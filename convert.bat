b05567feaed5d473e089bf45a5e704c44d0b861e
Kotet <kotet0418@gmail.com>
2024-03-12 10:40:34 +0900

    allow multiple files to be converted at once

--- convert.bat
+++ convert.bat
@@ -3,9 +3,16 @@
 @REM This script converts a text file dropped on it to a WAV file using coeiroink2-txt2wav
 
 set CONVERTER="%~dp0target\release\coeiroink2-txt2wav.exe"
+
+if [%1]==[] goto :eof
+:loop
+
 echo Input file: %1
 echo Output file: %1.wav
 
 %CONVERTER% predict -i %1 -o %1.wav
+
+shift
+if not [%1]==[] goto loop
 
 pause
