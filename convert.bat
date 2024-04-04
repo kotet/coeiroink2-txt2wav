c5108f6d705989294177eeb4716e45fd7cb83c92
Kotet <kotet0418@gmail.com>
2024-04-04 10:06:24 +0900

    convert.bat now generates mp3 files

--- convert.bat
+++ convert.bat
@@ -1,8 +1,9 @@
 @echo off
 
-@REM This script converts a text file dropped on it to a WAV file using coeiroink2-txt2wav
+@REM This script converts a text file dropped on it to a MP3 file using coeiroink2-txt2wav
 
 set CONVERTER="%~dp0target\release\coeiroink2-txt2wav.exe"
+set FDKAAC="fdkaac.exe"
 
 if [%1]==[] goto :eof
 :loop
@@ -11,6 +12,8 @@ echo Input file: %1
 echo Output file: %1.wav
 
 %CONVERTER% predict -i %1 -o %1.wav
+%FDKAAC% -b 200704 -o %1.m4a %1.wav
+del %1.wav
 
 shift
 if not [%1]==[] goto loop
